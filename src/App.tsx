8ca5d61c9fde9b08444bb998c0fff8a7e145ddf3
Poltergeist <spectator.networx@gmail.com>
2022-11-16 10:10:46 +0100

    feat: add basic list printing
    Currently this is only static, the lists need to be added into a file
    with a yaml string.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { parse } from "yaml";
+import deck from "./test.js";
+
+const parsedDeck = parse(deck);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Object.entries(parsedDeck).map(
+        ([title, list]) => (
+          <section className="decklist" key={title}>
+            <header>{title}</header>
+            <ul>
+              {Array.isArray(list)
+                ? list.map((card, index) => <li key={`${title}-${index}`}>{card}</li>)
+                : null}
+            </ul>
+          </section>
+        )
+      )}
     </div>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,46 @@
+const file = `Archaeology (3):
+  - Lightning-Core Excavator
+  - Prismite
+  - Archaeomender
+  - Archaeomender
+  - Ancestral Statue
+  - Juggernaut
+  - Vedalken Archmage
+  - Sharding Sphinx
+  - Meteor Golem
+  - Thirst for Knowledge
+  - Aether Spellbomb
+  - Hedron Archive
+  - Buried Ruin
+  - Island
+  - Island
+  - Island
+  - Island
+  - Island
+  - Island
+  - Thriving Isle
+
+Basri:
+  - Staunch Shieldmate
+  - Concordia Pegasus
+  - Tempered Veteran
+  - Siege Striker
+  - Basri's Acolyte
+  - Basri's Lieutenant
+  - Knight of the Tusk
+  - Basri Ket
+  - Feat of Resistance
+  - Basri's Solidarity
+  - Legion's Judgment
+  - Faith's Fetters
+  - Plains
+  - Plains
+  - Plains
+  - Plains
+  - Plains
+  - Plains
+  - Plains
+  - Thriving Heath`
+
+export default file;
+
